845564f667449e3a50963373dd0660f8dbc0c120
Avedis Demirdjian <avedis_demirdjian@epam.com>
2024-06-09 00:33:02 +0400

    Add carousel

--- main.js
+++ main.js
@@ -1,24 +1,44 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const images = document.querySelectorAll(".slide");
+const prevButton = document.querySelector(".previous");
+const nextButton = document.querySelector(".next");
+const selectorButtons = document.querySelectorAll(".selector-button");
+const learnMoreWrappers = document.querySelectorAll(".learn-more-wrapper");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+let index = 0;
+selectorButtons[index].style.background = "#6f6f6f";
+learnMoreWrappers[index].style.zIndex = 100;
+selectorButtons.forEach((b, i) => {
+  b.addEventListener("click", () => {
+    selectorButtons[index].style.background = "#d6d6d6";
+    delete images[index].dataset.active;
+    learnMoreWrappers[index].style.zIndex = 0;
+    index = i;
+    b.style.background = "#6f6f6f";
+    images[i].dataset.active = true;
+    learnMoreWrappers[i].style.zIndex = 100;
+  });
+});
+prevButton.addEventListener("click", () => {
+  delete images[index].dataset.active;
+  selectorButtons[index].style.background = "#d6d6d6";
+  learnMoreWrappers[index].style.zIndex = 0;
+  let previousIndex = --index;
+  if (previousIndex === -1) {
+    previousIndex = index = images.length - 1;
+  }
+  images[previousIndex].dataset.active = true;
+  learnMoreWrappers[previousIndex].style.zIndex = 100;
+  selectorButtons[previousIndex].style.background = "#6f6f6f";
+});
+nextButton.addEventListener("click", () => {
+  delete images[index].dataset.active;
+  selectorButtons[index].style.background = "#d6d6d6";
+  learnMoreWrappers[index].style.zIndex = 0;
+  let nextIndex = ++index;
+  if (nextIndex === images.length) {
+    nextIndex = index = 0;
+  }
+  images[nextIndex].dataset.active = true;
+  learnMoreWrappers[nextIndex].style.zIndex = 100;
+  selectorButtons[nextIndex].style.background = "#6f6f6f";
+});
