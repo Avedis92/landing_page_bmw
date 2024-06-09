7e68134e1618f49a34915863f3674b77fffda6ce
Avedis Demirdjian <avedis_demirdjian@epam.com>
2024-06-10 01:23:14 +0400

    Add carousel play pause button

--- main.js
+++ main.js
@@ -3,10 +3,14 @@ const prevButton = document.querySelector(".previous");
 const nextButton = document.querySelector(".next");
 const selectorButtons = document.querySelectorAll(".selector-button");
 const learnMoreWrappers = document.querySelectorAll(".learn-more-wrapper");
+const playButton = document.querySelector(".play-button");
+const pauseButton = document.querySelector(".pause-button");
 
 let index = 0;
+let timerId;
 selectorButtons[index].style.background = "#6f6f6f";
 learnMoreWrappers[index].style.zIndex = 100;
+pauseButton.style.display = "none";
 selectorButtons.forEach((b, i) => {
   b.addEventListener("click", () => {
     selectorButtons[index].style.background = "#d6d6d6";
@@ -41,4 +45,16 @@ nextButton.addEventListener("click", () => {
   images[nextIndex].dataset.active = true;
   learnMoreWrappers[nextIndex].style.zIndex = 100;
   selectorButtons[nextIndex].style.background = "#6f6f6f";
+});
+playButton.addEventListener("click", () => {
+  playButton.style.display = "none";
+  pauseButton.style.display = "block";
+  timerId = setInterval(() => {
+    nextButton.click();
+  }, 2000);
+});
+pauseButton.addEventListener("click", () => {
+  playButton.style.display = "block";
+  pauseButton.style.display = "none";
+  clearInterval(timerId);
 });
